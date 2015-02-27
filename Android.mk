303ef1fdd511f57f1fcce34e5a3568622fba77c1
Daniel <nieltg@users.noreply.github.com>
2015-02-27 22:10:42 +0700

    Fix & improve build system
    NOTE:
    - Improve build guard with multiple conditional statements
    - Hijack internal kernel builder by manipulating TARGET_KERNEL_SOURCE
    - Modify initramfs building mechanism (use 'init' as prerequisites)
    - Modify 'include' routine (bugfix: fail to include kernel2.mk)
    - kernel2: Resolve conflicts between kernel2.mk & internal kernel.mk
    - kernel2: Copy TARGET_AUTO_KDIR to prevent 'no kernel source' errors
    - kernel2: Work around exec-permission issue by using 'bash' prefix
    - kernel2: Add initramfs bypass hack for testing purpose (TODO: remove)

--- Android.mk
+++ Android.mk
@@ -1,26 +1,50 @@
-ifeq ($(TARGET_DEVICE),codina)
-
 LOCAL_PATH := $(call my-dir)
 
-$(warning codinaramfs: codinaramfs is not ready yet)
+CODINARAMFS_IS_ENABLED := true
+
+ifneq ($(TARGET_DEVICE),codina)
+$(warning codinaramfs: TARGET_DEVICE is not codina)
+CODINARAMFS_IS_ENABLED := 
+endif
+
+CODINARAMFS_KERNEL := test
+ifneq ($(TARGET_PREBUILT_KERNEL),test)
+$(warning codinaramfs: TARGET_PREBUILT_KERNEL is not CODINARAMFS_KERNEL)
+CODINARAMFS_IS_ENABLED := 
+endif
+
+ifeq ($(CODINARAMFS_IS_ENABLED),true)
+
+$(warning codinaramfs: codinaramfs is enabled)
 $(warning codinaramfs: should be used for testing purposes only)
+
+# Hijack kernel.mk so it doesn't build the kernel.
+
+ifdef TARGET_KERNEL_SOURCE
+CODINARAMFS_KERNEL_SOURCE := $(TARGET_KERNEL_SOURCE)
+endif
+
+TARGET_KERNEL_SOURCE := 
 
 # Prepare common variables.
 
 CODINARAMFS_OUT := $(TARGET_OUT_INTERMEDIATES)/CODINARAMFS_OBJ
 CODINARAMFS_KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/CODINARAMFS_KERNEL_OBJ
 
-CODINARAMFS_BUILD_TARGET :=
-
-# Include more files.
-
-include $(call first-makefiles-under,$(LOCAL_PATH))
-include kernel2.mk
+CODINARAMFS_OUT_INIT := $(CODINARAMFS_OUT)/init
 
 # Define final rules.
 
 $(CODINARAMFS_OUT):
 	mkdir -p $(CODINARAMFS_OUT)
 
-endif # if TARGET_DEVICE is codina
+# Include more files.
+
+include \
+	$(call first-makefiles-under, $(LOCAL_PATH)) \
+	$(LOCAL_PATH)/kernel2.mk
+
+else
+$(warning codinaramfs: codinaramfs is disabled)
+endif # if CODINARAMFS_IS_ENABLED is true
 
